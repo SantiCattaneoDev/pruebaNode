843cc86082372a9bab19e45b3f9779d5708587e1
SantiCattaneoDev <cattaneosantinou@gmail.com>
2023-01-05 16:57:51 -0300

    los archivos necesarios para realizar la publicacion en render fueron enviados

---
+++ MODULO_4/clase_17_DH - Sincronica/MercadoLiebre/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
